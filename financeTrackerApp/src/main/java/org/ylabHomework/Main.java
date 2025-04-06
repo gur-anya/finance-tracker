511b774fc527f9cf463f7695b68dd162276c4b5b
Gureva Anna Mikhailovna <149677477+anya-ananasss@users.noreply.github.com>
2025-04-06 05:20:03 +0300

    implemented aspects modules

--- financeTrackerApp/src/main/java/org/ylabHomework/Main.java
+++ financeTrackerApp/src/main/java/org/ylabHomework/Main.java
@@ -1,9 +1,11 @@
 package org.ylabHomework;
 
+import org.logging.EnableLogging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableLogging
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
